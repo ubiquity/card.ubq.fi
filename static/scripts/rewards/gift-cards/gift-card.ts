c91e644438be4faeea0272d3418cac8de504e483
EresDev <eresdev@hotmail.com>
2025-06-03 23:01:09 +0500

    fix: make catalog product a link to product page

--- static/scripts/rewards/gift-cards/gift-card.ts
+++ static/scripts/rewards/gift-cards/gift-card.ts
@@ -100,69 +100,71 @@ function getRangePricesHtml(giftCard: GiftCard, rewardAmount: BigNumberish) {
 export function getSingleGiftCardHtml(product: Product) {
   // Changed type from GiftCard to Product
   return html`
-    <div class="card-section" id="offered-card" data-product-id="${product.productId}">
-      <div class="card-image-container">
-        <img src="${product.logoUrls[0]}" alt="${product.productName}" />
-      </div>
-      <div class="details">
-        <h3>${product.productName}</h3>
-        <div class="pricing">
-          <div class="available">
-            ${product.denominationType === "FIXED"
-              ? html`
-                  <div class="fixed-denominations">
-                    ${product.fixedRecipientDenominations
-                      .map(
-                        (amount) => html`
-                          <div class="amount-option">
-                            <span class="currency">${product.recipientCurrencyCode}</span>
-                            <span class="amount">${amount}</span>
-                          </div>
-                        `
-                      )
-                      .join("")}
-                  </div>
-                `
-              : html`
-                  <div class="range-denominations">
-                    <div class="amount-range">
-                      <span class="currency">${product.recipientCurrencyCode}</span>
-                      <span class="amount">${product.minRecipientDenomination}</span>
-                      -
-                      <span class="currency">${product.recipientCurrencyCode}</span>
-                      <span class="amount">${product.maxRecipientDenomination}</span>
-                    </div>
-                  </div>
-                `}
-          </div>
+    <a href="#/${product.productId}">
+      <div class="card-section" id="offered-card" data-product-id="${product.productId}">
+        <div class="card-image-container">
+          <img src="${product.logoUrls[0]}" alt="${product.productName}" />
         </div>
+        <div class="details">
+          <h3>${product.productName}</h3>
+          <div class="pricing">
+            <div class="available">
+              ${product.denominationType === "FIXED"
+                ? html`
+                    <div class="fixed-denominations">
+                      ${product.fixedRecipientDenominations
+                        .map(
+                          (amount) => html`
+                            <div class="amount-option">
+                              <span class="currency">${product.recipientCurrencyCode}</span>
+                              <span class="amount">${amount}</span>
+                            </div>
+                          `
+                        )
+                        .join("")}
+                    </div>
+                  `
+                : html`
+                    <div class="range-denominations">
+                      <div class="amount-range">
+                        <span class="currency">${product.recipientCurrencyCode}</span>
+                        <span class="amount">${product.minRecipientDenomination}</span>
+                        -
+                        <span class="currency">${product.recipientCurrencyCode}</span>
+                        <span class="amount">${product.maxRecipientDenomination}</span>
+                      </div>
+                    </div>
+                  `}
+            </div>
+          </div>
 
-        <button id="mint" class="btn" data-loading="false">
-          <div class="action">Mint</div>
-          <div class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" id="claim-icon">
-              <path
-                d="M252.309-180.001q-30.308 0-51.308-21t-21-51.308V-360H240v107.691q0 4.616 3.846 8.463 3.847 3.846 8.463 3.846h455.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-360h59.999v107.691q0 30.308-21 51.308t-51.308 21H252.309ZM480-335.386 309.233-506.153l42.153-43.383 98.615 98.615v-336.001h59.998v336.001l98.615-98.615 42.153 43.383L480-335.386Z"
-              ></path>
-            </svg>
-          </div>
-          <div class="loader">
-            <svg
-              version="1.1"
-              id="L9"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              width="33.33"
-              height="33.33"
-              viewBox="0 0 100 100"
-              enable-background="new 0 0 0 0"
-              xml:space="preserve"
-            >
-              <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"></path>
-            </svg>
-          </div>
-        </button>
+          <button id="mint" class="btn" data-loading="false">
+            <div class="action">Mint</div>
+            <div class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" id="claim-icon">
+                <path
+                  d="M252.309-180.001q-30.308 0-51.308-21t-21-51.308V-360H240v107.691q0 4.616 3.846 8.463 3.847 3.846 8.463 3.846h455.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-360h59.999v107.691q0 30.308-21 51.308t-51.308 21H252.309ZM480-335.386 309.233-506.153l42.153-43.383 98.615 98.615v-336.001h59.998v336.001l98.615-98.615 42.153 43.383L480-335.386Z"
+                ></path>
+              </svg>
+            </div>
+            <div class="loader">
+              <svg
+                version="1.1"
+                id="L9"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="33.33"
+                height="33.33"
+                viewBox="0 0 100 100"
+                enable-background="new 0 0 0 0"
+                xml:space="preserve"
+              >
+                <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"></path>
+              </svg>
+            </div>
+          </button>
+        </div>
       </div>
-    </div>
+    </a>
   `;
 }
