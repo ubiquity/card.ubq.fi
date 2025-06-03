42ef141a000fc2ecee0ee888e8c9d361e72304c2
EresDev <eresdev@hotmail.com>
2025-06-03 15:22:36 +0500

    fix: remove redundant info from catalog

--- static/scripts/rewards/gift-cards/gift-card.ts
+++ static/scripts/rewards/gift-cards/gift-card.ts
@@ -1,5 +1,5 @@
 import { BigNumberish } from "ethers";
-import { GiftCard } from "../../../../shared/types";
+import { GiftCard, Product } from "../../../../shared/types";
 import { getFixedPriceToValueMap, getGiftCardValue, isRangePriceGiftCardClaimable } from "../../../../shared/pricing";
 import { formatEther } from "ethers/lib/utils";
 import { getGiftCardActivateInfoHtml } from "./activate/activate-html";
@@ -97,29 +97,59 @@ function getRangePricesHtml(giftCard: GiftCard, rewardAmount: BigNumberish) {
   return _html;
 }
 
-export function getSingleGiftCardHtml(giftCard: GiftCard) {
+export function getSingleGiftCardHtml(product: Product) {
+  // Changed type from GiftCard to Product
   return html`
-    <div class="card-section" id="offered-card" data-product-id="${giftCard.productId}">
-      <div>
-        <img src="${giftCard.logoUrls}" alt="${giftCard.productName}" />
+    <div class="card-section" id="offered-card" data-product-id="${product.productId}">
+      <div class="card-image-container">
+        <img src="${product.logoUrls[0]}" alt="${product.productName}" />
       </div>
       <div class="details">
-        <h3>${giftCard.productName}</h3>
+        <h3>${product.productName}</h3>
+        <p class="brand-name">${product.brand.brandName}</p>
 
-        <div class="pricing ${giftCard.denominationType}">
-          ${giftCard.denominationType == "FIXED"
-            ? giftCard.fixedRecipientDenominations.join(", ")
-            : giftCard.minRecipientDenomination + " - " + giftCard.maxRecipientDenomination}
+        <div class="pricing">
+          <div class="available">
+            ${product.denominationType === "FIXED"
+              ? html`
+                  <div class="fixed-denominations">
+                    ${product.fixedRecipientDenominations
+                      .map(
+                        (amount) => html`
+                          <div class="amount-option">
+                            <span class="currency">${product.recipientCurrencyCode}</span>
+                            <span class="amount">${amount}</span>
+                          </div>
+                        `
+                      )
+                      .join("")}
+                  </div>
+                `
+              : html`
+                  <div class="range-denominations">
+                    <div class="amount-range">
+                      <span class="currency">${product.recipientCurrencyCode}</span>
+                      <span class="amount">${product.minRecipientDenomination}</span>
+                      -
+                      <span class="currency">${product.recipientCurrencyCode}</span>
+                      <span class="amount">${product.maxRecipientDenomination}</span>
+                    </div>
+                  </div>
+                `}
+          </div>
         </div>
-        <div>SKU: ${giftCard.productId}</div>
+
+        <div class="product-sku">SKU: ${product.productId}</div>
+
         <button id="mint" class="btn" data-loading="false">
           <div class="action">Mint</div>
-          <div class="icon"
-            ><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" id="claim-icon">
+          <div class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" id="claim-icon">
               <path
                 d="M252.309-180.001q-30.308 0-51.308-21t-21-51.308V-360H240v107.691q0 4.616 3.846 8.463 3.847 3.846 8.463 3.846h455.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-360h59.999v107.691q0 30.308-21 51.308t-51.308 21H252.309ZM480-335.386 309.233-506.153l42.153-43.383 98.615 98.615v-336.001h59.998v336.001l98.615-98.615 42.153 43.383L480-335.386Z"
-              ></path></svg
-          ></div>
+              ></path>
+            </svg>
+          </div>
           <div class="loader">
             <svg
               version="1.1"
@@ -132,11 +162,11 @@ export function getSingleGiftCardHtml(giftCard: GiftCard) {
               enable-background="new 0 0 0 0"
               xml:space="preserve"
             >
-              <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"></path></svg
-          ></div>
+              <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"></path>
+            </svg>
+          </div>
         </button>
       </div>
     </div>
-    ${getGiftCardActivateInfoHtml(giftCard)}
   `;
 }
